4d2d1314e7769d998430cdb5e332386ab26a1f83
amirfarzamnia <amir2452fa@gmail.com>
2024-04-13 16:22:26 +0200

    update

--- public/bot/index.js
+++ public/bot/index.js
@@ -1,3 +1,13 @@
+if (!window.sessionStorage.getItem('session')) {
+    const buffer = new Uint8Array(16);
+
+    window.crypto.getRandomValues(buffer);
+
+    const hashArray = Array.from(buffer).map((byte) => byte.toString(16).padStart(2, '0'));
+
+    window.sessionStorage.setItem('session', hashArray.join(''));
+}
+
 fetch(window.location.origin + '/api/fetch_bot' + window.location.search, {
     method: 'GET',
     headers: {
