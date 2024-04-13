191e97c5ef1dcb2e2de8c881c5d8b6d8b12f4131
amirfarzamnia <amir2452fa@gmail.com>
2024-04-13 16:29:01 +0200

    update

--- public/bot/index.js
+++ public/bot/index.js
@@ -1,22 +1,30 @@
+// Checking if the session exists in the browser's session storage.
 if (!window.sessionStorage.getItem('session')) {
-    const buffer = new Uint8Array(16);
+    // Creating a buffer of unit 8 arrays with 32 characters.
+    const buffer = new Uint8Array(32);
 
+    // Getting a random value from the buffer using crypto.
     window.crypto.getRandomValues(buffer);
 
+    // Creating a hash array as the session.
     const hashArray = Array.from(buffer).map((byte) => byte.toString(16).padStart(2, '0'));
 
+    // Setting the browser's session storage's session.
     window.sessionStorage.setItem('session', hashArray.join(''));
 }
 
+// Sending a request to fetch the information of the requested robot on the page.
 fetch(window.location.origin + '/api/fetch_bot' + window.location.search, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
+    // Using the GET method to send the request.
+    method: 'GET'
+    // Then fetching the received response from the request.
 }).then(async (res) => {
+    // Check if the response status code is 200 (ok).
     if (res.status === 200) {
+        // Extracting the bot parameters from the response.
         const { id, name, description } = await res.json();
 
+        // Setting the document's inner HTML based on the received parameters.
         document.body.innerHTML = /* html */ `
         <div class="container">
             <h1>${name}</h1>
