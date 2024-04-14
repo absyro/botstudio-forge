56b21410993d7b3bf0dad88d65d06491028f1b47
amirfarzamnia <amir2452fa@gmail.com>
2024-04-14 22:04:23 +0200

    update

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,4 @@
 import '../assets/javascript/pace.js';
+
+// If the theme is not defined, which means the user is new to the website, set the default theme mode based on what the what the system prefers. Dark or light.
+if (!window.localStorage.getItem('theme-mode')) window.localStorage.setItem('theme-mode', window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
